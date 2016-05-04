d616980cee1d6085f3fe1707299be7c7d8843e36
HaojunZou <apollo_zhj@msn.com>
2016-05-04 17:02:41 +0200

    crm_repord: crm_repord_view updated

--- crm_repord/__openerp__.py
+++ crm_repord/__openerp__.py
@@ -29,7 +29,7 @@ CRM Repord for mobile device
 """,
     'author': 'Vertel AB',
     'website': 'http://www.vertel.se',
-    'depends': ['base', 'sale', 'product', 'website', 'edi_gs1', 'calendar_ics', 'calendar_kanban', 'crm_meeting', 'crm_campaign'], #Maybe not campaign, but one of it's dependencies?
+    'depends': ['base', 'sale', 'product', 'website', 'edi_gs1', 'website_product_category' 'calendar_ics', 'calendar_kanban', 'crm_meeting', 'crm_campaign'], #Maybe not campaign, but one of it's dependencies?
     'data': [
         'views/crm_repord_view.xml',
         'views/crm_repord_data.xml',
