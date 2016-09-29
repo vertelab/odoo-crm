6d88a888d71d7807ff19f32b84ac6eb73bbfec05
Haojun Zou <apollo_zhj@msn.com>
2016-09-29 14:48:18 +0200

    website_crm_partner: refactory 2

--- website_crm_partner/res_partner.py
+++ website_crm_partner/res_partner.py
@@ -37,51 +37,51 @@ class res_partner(models.Model):
 
 class website_crm_partner(http.Controller):
 
-    @http.route([
-    MODULE_BASE_PATH + '<model("res.partner"):partner>',
-    MODULE_BASE_PATH,
-    MODULE_BASE_PATH + 'add',
-    MODULE_BASE_PATH + 'delete',
-    MODULE_BASE_PATH + '<model("res.partner"):partner>/edit',
-    MODULE_BASE_PATH + 'set_login',
-    ], type='http', auth="public", website=True)
-    def get_partner(self, partner=False, **post):
-        if request.httprequest.url[-4:] == 'edit': #edit form
-            return request.render('website_crm_partner.partner_edit', {'partner': partner, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'edit'})
-        if request.httprequest.url[-3:] == 'add': #add form
-            return request.render('website_crm_partner.partner_edit', {'partner': None, 'root': MODULE_BASE_PATH, 'db': request.db,})
-        if request.httprequest.url[-9:] == 'set_login': #set login form
-            if request.httprequest.method == 'POST':
-                return werkzeug.utils.redirect(MODULE_BASE_PATH, 302)
-            return request.render('website_crm_partner.set_login', {'partner': None, 'root': MODULE_BASE_PATH, 'db': request.db,})
-        if request.httprequest.method == 'POST':
-            if post.get('id') == '' and post.get('btn-save') == 'btn-save': #new partner
-                partner = request.env['res.partner'].create({
-                    'name': post.get('name'),
-                    'type': post.get('type'),
-                    'comment': post.get('comment', ''),
-                })
-                return request.render('website_crm_partner.partner_detail', {'partner': partner, 'root': MODULE_BASE_PATH, 'db': request.db,})
-            elif post.get('id') != '' and post.get('btn-save') == 'btn-save': #exist partner
-                partner = request.env['res.partner'].browse(int(post.get('id')))
-                partner.write({
-                    'name': post.get('name'),
-                    'comment': post.get('comment', ''),
-                })
-                return request.render('website_crm_partner.partner_detail', {'partner': partner, 'root': MODULE_BASE_PATH, 'db': request.db,})
-            elif post.get('search_words') != '' and post.get('btn-search') == 'btn-search': #search
-                partners = request.env['res.partner'].search(['&', ('name', 'ilike', post['search_words']), ('type', '=', 'contact')], order='name',limit=25)
-                return request.render('website_crm_partner.partner_list', {'partners': partners, 'root': MODULE_BASE_PATH, 'db': request.db,})
-            return werkzeug.utils.redirect(MODULE_BASE_PATH, 302)
-        else:
-            if partner:
-                if partner and post.get('btn-delete') == 'btn-delete': #delete
-                    partner.unlink()
-                    return werkzeug.utils.redirect(MODULE_BASE_PATH, 302)
-                return request.render('website_crm_partner.partner_edit', {'partner': partner, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'view'})
-            else:
-                partners = request.env['res.partner'].search([('type', '=', 'contact')], order='name',limit=25)
-                return request.render('website_crm_partner.partner_list', {'partners': partners, 'root': MODULE_BASE_PATH, 'db': request.db,})
+    #~ @http.route([
+    #~ MODULE_BASE_PATH + '<model("res.partner"):partner>',
+    #~ MODULE_BASE_PATH,
+    #~ MODULE_BASE_PATH + 'add',
+    #~ MODULE_BASE_PATH + 'delete',
+    #~ MODULE_BASE_PATH + '<model("res.partner"):partner>/edit',
+    #~ MODULE_BASE_PATH + 'set_login',
+    #~ ], type='http', auth="public", website=True)
+    #~ def get_partner(self, partner=False, **post):
+        #~ if request.httprequest.url[-4:] == 'edit': #edit form
+            #~ return request.render('website_crm_partner.partner_edit', {'partner': partner, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'edit'})
+        #~ if request.httprequest.url[-3:] == 'add': #add form
+            #~ return request.render('website_crm_partner.partner_edit', {'partner': None, 'root': MODULE_BASE_PATH, 'db': request.db,})
+        #~ if request.httprequest.url[-9:] == 'set_login': #set login form
+            #~ if request.httprequest.method == 'POST':
+                #~ return werkzeug.utils.redirect(MODULE_BASE_PATH, 302)
+            #~ return request.render('website_crm_partner.set_login', {'partner': None, 'root': MODULE_BASE_PATH, 'db': request.db,})
+        #~ if request.httprequest.method == 'POST':
+            #~ if post.get('id') == '' and post.get('btn-save') == 'btn-save': #new partner
+                #~ partner = request.env['res.partner'].create({
+                    #~ 'name': post.get('name'),
+                    #~ 'type': post.get('type'),
+                    #~ 'comment': post.get('comment', ''),
+                #~ })
+                #~ return request.render('website_crm_partner.partner_detail', {'partner': partner, 'root': MODULE_BASE_PATH, 'db': request.db,})
+            #~ elif post.get('id') != '' and post.get('btn-save') == 'btn-save': #exist partner
+                #~ partner = request.env['res.partner'].browse(int(post.get('id')))
+                #~ partner.write({
+                    #~ 'name': post.get('name'),
+                    #~ 'comment': post.get('comment', ''),
+                #~ })
+                #~ return request.render('website_crm_partner.partner_detail', {'partner': partner, 'root': MODULE_BASE_PATH, 'db': request.db,})
+            #~ elif post.get('search_words') != '' and post.get('btn-search') == 'btn-search': #search
+                #~ partners = request.env['res.partner'].search(['&', ('name', 'ilike', post['search_words']), ('type', '=', 'contact')], order='name',limit=25)
+                #~ return request.render('website_crm_partner.partner_list', {'partners': partners, 'root': MODULE_BASE_PATH, 'db': request.db,})
+            #~ return werkzeug.utils.redirect(MODULE_BASE_PATH, 302)
+        #~ else:
+            #~ if partner:
+                #~ if partner and post.get('btn-delete') == 'btn-delete': #delete
+                    #~ partner.unlink()
+                    #~ return werkzeug.utils.redirect(MODULE_BASE_PATH, 302)
+                #~ return request.render('website_crm_partner.partner_edit', {'partner': partner, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'view'})
+            #~ else:
+                #~ partners = request.env['res.partner'].search([('type', '=', 'contact')], order='name',limit=25)
+                #~ return request.render('website_crm_partner.partner_list', {'partners': partners, 'root': MODULE_BASE_PATH, 'db': request.db,})
 
 
     @http.route([
@@ -94,30 +94,30 @@ class website_crm_partner(http.Controller):
     MODULE_BASE_PATH + 'set_login',
     ], type='http', auth="public", website=True)
     def get_partner(self, partner=None, search='',**post):
-        
+
         search_domain = [('type','=','contact')]
+        model = 'res.partner'
         fields =  ['name','phone','email']
         template = {'list': 'website_crm_partner.partner_list', 'detail': 'website_crm_partner.partner_detail'}
-        
+
+
         if request.httprequest.url[-4:] == 'edit': #Edit
             if request.httprequest.method == 'GET':
-                return request.render(template['detail'], {'partner': partner, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'edit'})            
+                return request.render(template['detail'], {'model': model, 'partner': partner, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'edit'})
             else:
                 partner.write({
-                    'name': post.get('name'),
-                    'comment': post.get('comment', ''),
+                    f: post.get(f) for f in fields
                 })
-                return request.render(template['detail'], {'partner': partner, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'view'})         
+                return request.render(template['detail'], {'model': model, 'partner': partner, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'view'})
+
         elif request.httprequest.url[-3:] == 'add': #Add
             if request.httprequest.method == 'GET':
-                return request.render(template['detail'], {'partner': None, 'root': MODULE_BASE_PATH, 'db': request.db,'mode': 'edit'})
+                return request.render(template['detail'], {'model': model, 'partner': None, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db,'mode': 'edit'})
             else:
-                partner = request.env['res.partner'].create({
-                    'name': post.get('name'),
-                    'type': post.get('type'),
-                    'comment': post.get('comment', ''),
-                })
-                return request.render(template['detail'], {'partner': partner, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'view'})         
+                record = { f: post.get(f) for f in fields }
+                record['type'] = 'contact'
+                partner = request.env['res.partner'].create(record)
+                return request.render(template['detail'], {'model': model, 'partner': partner, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'view'})
         elif request.httprequest.url[-6:] == 'delete': #Delete
             partner.unlink()
         elif request.httprequest.url[-6:] == 'search': #Search
@@ -125,21 +125,21 @@ class website_crm_partner(http.Controller):
                 search = post.get('search')
             search_domain.append(('name','like',search))
         elif partner:  # Detail
-            return request.render(template['detail'], {'partner': partner, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'view'}) 
-        
+            return request.render(template['detail'], {'model': model, 'partner': partner, 'fields': fields, 'root': MODULE_BASE_PATH, 'db': request.db, 'mode': 'view'})
+
         return request.render(template['list'], {
-            'partners': request.env['res.partner'].search(search_domain, order='name',limit=25), 
-            'root': MODULE_BASE_PATH, 
+            'partners': request.env['res.partner'].search(search_domain, order='name',limit=25),
+            'root': MODULE_BASE_PATH,
             'db': request.db,
         })
-        
+
 #######################
-        
+
         if request.httprequest.url[-9:] == 'set_login': #set login form
             if request.httprequest.method == 'POST':
                 return werkzeug.utils.redirect(MODULE_BASE_PATH, 302)
             return request.render('website_crm_partner.set_login', {'partner': None, 'root': MODULE_BASE_PATH, 'db': request.db,})
-        
+
 
 
     @http.route(['/mobile/security/<model("res.partner"):partner>', '/mobile/security'], type='http', auth="user", website=True)
