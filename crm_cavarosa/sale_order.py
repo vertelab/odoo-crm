3a3fcc9f9c459826348b19318cb0b94d3f995735
Haojun Zou <apollo_zhj@msn.com>
2017-02-08 20:32:02 +0100

    crm_cavarosa: use crm.case.tags

--- crm_cavarosa/sale_order.py
+++ crm_cavarosa/sale_order.py
@@ -29,6 +29,7 @@ class sale_order_generator(models.TransientModel):
     _name = 'sale.order.generator'
 
     order_id = fields.Many2one(comodel_name='sale.order', string='Sale Order', required=True)
+    categ_ids = fields.Many2many(comodel_name='crm.case.categ', string='Tags', required=True)
     @api.model
     def get_default_partner_ids(self):
         return self.env['res.partner'].browse(self._context.get('active_ids', []))
@@ -36,24 +37,9 @@ class sale_order_generator(models.TransientModel):
 
     @api.one
     def generate_sale_orders(self):
-        categs = self.get_so_categs(self.partner_ids)
+        categs = self.categ_ids
         for p in self.partner_ids:
             values = self.order_id.onchange_partner_id(p.id)['value']
             values['partner_id'] = p.id
             order = self.order_id.copy(values)
             order.categ_ids = categs
-
-    @api.model
-    def get_so_categs(self, partners):
-        rp_categs = partners[0].category_id
-        for rp in partners[1:]:
-            rp_categs &= rp.category_id
-        so_categs = self.env['crm.case.categ'].browse([])
-        if len(rp_categs) > 0:
-            for rpc in rp_categs.mapped('name'):
-                exist_categ = self.env['crm.case.categ'].search([]).filtered(lambda sc: sc.name == rpc)
-                if len(exist_categ) > 0:
-                    so_categs |= exist_categ[0]
-                else:
-                    so_categs |= self.env['crm.case.categ'].create({'name': rpc})
-        return so_categs
