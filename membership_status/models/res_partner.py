a8a7353e150bbe7b22466ee7c557e750c93f0ba1
Michael Tanzi <michael.tanzi@vertel.se>
2020-06-15 06:47:18 +0000

    odoo-crm: membership_status: added recruitment workflow and admission status

--- membership_status/models/res_partner.py
+++ membership_status/models/res_partner.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from odoo import models, fields, api, _
+from odoo import models, fields, api,SUPERUSER_ID, _
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -26,7 +26,31 @@ _logger = logging.getLogger(__name__)
 class res_partner(models.Model):
     _inherit = "res.partner"
     
-    membership_recruitment_status_id = fields.Many2one(comodel_name = "membership.recruitment.status")
+    def _get_default_status_id(self):
+        """ Gives default stage_id """
+        partner_id = self.env.context.get('default_partner_id')
+        if not partner_id:
+            return False
+        return self.env['membership.recruitment.status'].search([('found','=',False)])[0]
+
+    @api.model
+    def _read_group_status_ids(self, stages, domain, order):
+        return self.env['membership.recruitment.status'].search([])
+        search_domain = [('id', 'in', stages.ids)]
+        stage_ids = stages._search(search_domain, order=order, access_rights_uid=SUPERUSER_ID)
+        return stages.browse(stage_ids)
+    
+    membership_recruitment_status_id = fields.Many2one(comodel_name = "membership.recruitment.status",
+                                                        ondelete='restrict', track_visibility='onchange', index=True,
+                                                        default=_get_default_status_id, group_expand='_read_group_status_ids', copy=False)
+    
+    @api.multi
+    def start_admission(self):
+        for partner in self:
+            partner.membership_recruitment_status_id = self.env['membership.recruitment.status'].search([])[0]
+        action = self.env.ref("membership_status.action_admission_form")
+        return action
+ 
  
 class membership_recruitment_status(models.Model):
     _name = "membership.recruitment.status"
