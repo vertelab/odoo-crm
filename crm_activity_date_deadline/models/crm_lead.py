9e954826e864f9ac30c23000637ec4485341262e
Jakob Krabbe <jakob.krabbe@vertel.se>
2024-03-27 14:17:17 +0100

    crm_activity_date_deadline: see odoo-project > customer_project_user / store=True

--- crm_activity_date_deadline/models/crm_lead.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-
-class ResCompany(models.Model):
-    _inherit = 'crm.lead'
-  
-    active_date_deadline = fields.Date().stored = True
-    #linkedin_client_id = fields.Char(string='Client ID')
-    #linkedin_client_secret = fields.Char(string='Client Secret')
