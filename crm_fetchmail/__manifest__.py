c598650965c47bbd4c7715d3db6349b22d71ef48
Anders Wallenquist <anders.wallenquist@vertel.se>
2026-05-06 11:08:12 +0000

    crm_fetchmail: new module

---
+++ crm_fetchmail/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'CRM Fetch Mail Action',
+    'version': '1.0',
+    'category': 'Sales/CRM',
+    'summary': 'Adds a server-action for fetching mail',
+    'depends': ['crm', 'mail'],
+    'data': [
+        'data/server_action.xml',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
---
+++ crm_fetchmail/models/crm_lead.py
@@ -0,0 +1,12 @@
+from odoo import models, api
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    @api.model
+    def action_force_fetch_mail(self):
+        # Hämtar alla konfigurerade inkommande e-postservrar
+        mail_servers = self.env['fetchmail.server'].search([('state', '=', 'done')])
+        for server in mail_servers:
+            server.fetch_mail()
+        return True
